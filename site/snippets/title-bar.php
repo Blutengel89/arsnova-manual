b7f946b2ca46b31bb977f6857088ee0ea887e408
Sonja Broda <sonja@imap.cc>
2015-01-03 13:59:08 +0100

    add buttons for view and back

--- site/snippets/title-bar.php
+++ site/snippets/title-bar.php
@@ -1,10 +1,20 @@
 
 <header class="header" role="navigation" aria-label="header">
     <div class="container">
-     
+    		<?php if($page->template() == 'instructions') : ?><a class="btn btn-default pull-left" href="<?php echo $page->parent()->url() ?>" role="button"><?php echo l('back') ?></a><?php endif ?>
         <a class="brand" href="<?php echo url() ?>">
         	<span class="icon-radar"></span><span class="icon-ars"></span><span class="icon-nova"></span><span><?php echo $pages->find('start')->title() ?></span>   
         </a>
+        <button type="button" class="btn btn-default pull-right"><?php echo l('ansicht'); ?></button>
+					<!--div class"btn-group">
+					<?php if($page->hasNext()): ?> 
+						<a class="btn btn-default pull-right" href="<?php echo $page->next()->url() ?>"><?php echo l('next') ?></a>
+					<?php  endif; ?>
+					<?php if($page->hasPrev()): ?>
+						<a class="btn btn-default pull-right" href="<?php echo $page->prev()->url() ?>"><?php echo l('back') ?></a>
+					<?php endif; ?>
+					</div-->
+        
 
    </div>
 </header>
