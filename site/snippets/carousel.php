d51b3c30de6bb6d8ec1249695cb581573a1073f2
Sonja Broda <sonja@imap.cc>
2015-01-05 19:39:06 +0100

    moved text before image

--- site/snippets/carousel.php
+++ site/snippets/carousel.php
@@ -1,13 +1,16 @@
-<div id="carousel-example-<?php echo $page->uid();?>" class="carousel slide" data-interval="false" data-wrap="true">
+<div id="carousel-<?php echo $page->uid();?>" class="carousel slide" data-interval="false" data-wrap="true">
+	<ol class="carousel-indicators">
+		<?php for ($i = 1; $i <= $count; $i++):?>
+          <li data-target="#carousel-<?php echo $page->uid();?>" data-slide-to="<?php echo $i?>" class="<?php if ($i == 1): echo 'active'; endif;?>"></li>
+  		<?php endfor ?>
+  	</ol>
 	<!-- Wrapper for slides -->
 	<div class="toggle-temp carousel-inner">
 		<?php 
 		//Loop through images and add instructions from yaml-fields for each image
 			$image = $images->first();
 			foreach($instructions as $instruction) :?>
-				<div class="item <?php if ($count == 1): echo 'active'; endif;?>">
-					<img class="screenshot" src="<?php echo $image->url() ?>" alt="<?php echo $page->title() . "-" . $count ?>" /> 
-						<hr />
+				<div class="item <?php if($count == 1): echo 'active'; endif;?>">
 						<div class="carousel-caption">
 							<?php if($instruction['instruction'] != ""): ?>
 								<div class="instruction-item clearfix">	
@@ -18,15 +21,16 @@
 								<div class="result-text"><?php echo markdown($instruction['result']) ?></div>
 							<?php endif; ?>
 						</div>
+						<img class="screenshot" src="<?php echo $image->url() ?>" alt="<?php echo $page->title() . "-" . $count ?>" /> 
 				</div>
 				<?php $count++; $image=$images->next();
 			endforeach;?>
 	</div>
 	<!-- Controls -->
-	<a class="left carousel-control" href="#carousel-example-<?php echo $page->uid();?>" data-slide="prev">
+	<a class="left carousel-control" href="#carousel-<?php echo $page->uid();?>" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>
-	<a class="right carousel-control" href="#carousel-example-<?php echo $page->uid();?>" data-slide="next">
+	<a class="right carousel-control" href="#carousel-<?php echo $page->uid();?>" data-slide="next">
 		<span class="glyphicon glyphicon-chevron-right"></span>
 	</a>		
 </div>
