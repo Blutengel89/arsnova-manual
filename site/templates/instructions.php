8c585d3639bea0ec7921d9e9024374608ae4542f
Sonja Broda <sonja@imap.cc>
2015-01-05 12:04:48 +0100

    changed default button hover styles, make view button visible on larger screens only

--- site/templates/instructions.php
+++ site/templates/instructions.php
@@ -1,9 +1,9 @@
 <?php snippet('header') ?>
 <div id="main-content" class="container main-content toggle-carousel">
-		<button type="button" class="btn template"><?php echo l::get('ansicht'); ?></button>
+		<button type="button" class="btn btn-default template pull-right"><?php echo l::get('ansicht'); ?></button>
 			<h2><a href="<?php echo $page->parent()->url()?>"><span class="overview icon-menu pull-left"><span class="sr-only"><?php echo l('uebersicht') ?></span></span></a><?php echo $page->title()->html(); ?></h2>
 			<div class="container instructions">
-				
+			<?php echo (cookie::get('view')); ?>	
 				<?php
 					if(cookie::get('view') == 'carousel') { 
 						snippet('carousel', array('images' => $images, 'count' => $count, 'instructions' => $instructions));
