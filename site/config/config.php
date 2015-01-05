0ceb201232510c34cebade1ae921fe22bc6e31ea
Sonja Broda <sonja@imap.cc>
2015-01-05 23:44:41 +0100

    changed markdown break to false

--- site/config/config.php
+++ site/config/config.php
@@ -30,7 +30,7 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 */
 c::set('content.file.extension', 'md');
 c::set('debug', true);
-c::set('markdown.extra', true);
+c::set('markdown.extra', false);
 c::set('markdown.breaks', true);
 c::set('timezone', 'UTC');
 c::set('home', 'start');
