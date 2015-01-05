a18def5accb780bd2583b6fd47b6e2a939bdcf77
Sonja Broda <sonja@imap.cc>
2015-01-05 21:49:45 +0100

    rem jquery loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,9 @@
-
-$(document).ready(function() {
-	$("a:not(.carousel-control)").click(function() {
-		var page = $(this).attr('href');
-		$('#main-content').load(page);
-		return false;
-	});
-});
-
-
     $('.template').click(function(){
         $('.toggle-temp').toggleClass('carousel-inner');
         $('.container').toggleClass('toggle-carousel').toggleClass('toggle-list');
         $('.carousel-control').toggleClass('hidden');
-        if($.cookie("view") == 'carousel') {
-            $.cookie('view', 'list', {path: '/'});
-
-             $('.carousel-caption').each(function() {
-                    $(this).insertBefore( $(this).prev('.screenshot') );
-                    });        
-        }else{
-            $('.screenshot').each(function() {
-                    $(this).insertBefore( $(this).prev('.carousel-caption') );
-                    }); 
-            $.cookie('view', 'carousel', {path: '/'});
-        }
+        $('.carousel-indicators').toggleClass('hidden');
+        
     });
+
+
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,7 +1,6 @@
 		<?php 
 			echo js('//code.jquery.com/jquery.js');
 			echo js('//netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js');
-			echo js('assets/js/jquery.cookie.js');
 			echo js('assets/js/script.js');
 		?>
 	</body>
