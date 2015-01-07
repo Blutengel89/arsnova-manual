90b6ce2a2e1d5d49c72454304c1ebaea7ab713ee
Sonja Broda <sonja@imap.cc>
2015-01-07 14:41:33 +0100

    added script to prevent iPhone from opening safari on clicking links

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,13 @@ $('.template').click(function(){
 		$.cookie("view", "carousel");	
 	}		
 });
+
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
