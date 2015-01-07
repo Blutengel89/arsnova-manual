2145e8f3b9a19df65fdb4bcf45c6d639804dde74
Sonja Broda <sonja@imap.cc>
2015-01-07 12:45:43 +0100

    rem jquery load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 
 $('.template').click(function(){
-	console.log(window.location.href);
 	$('.carousel-control').toggleClass('hidden');
 	$('.toggle-temp').toggleClass('carousel-inner');
 	$('.container').toggleClass('toggle-carousel').toggleClass('toggle-list');
 	$('.carousel-indicators').toggleClass('hidden');
-	var url = $(location).attr('href');
 	var cookieValue = $.cookie("view");
 	if(cookieValue == 'carousel') {
 		$.cookie("view", "list");
@@ -14,10 +12,3 @@ $('.template').click(function(){
 		$.cookie("view", "carousel");	
 	}		
 });
-
-
-	$("a:not(.carousel-control)").click(function() {
- 		var page = $(this).attr('href');
- 		$('body').load(page);
- 		
- 	});
