d11d187e3a5732717bb2cc9b863eb3962c98ce6e
Sonja Broda <sonja@imap.cc>
2015-01-08 01:31:05 +0100

    fixed html bugs and container overflow on subnavigation

--- site/templates/sub-navigation.php
+++ site/templates/sub-navigation.php
@@ -1,6 +1,6 @@
 <?php if(!r::ajax()) {snippet('header');} ?>
 <div id="main-content" class="container main-content">
-<button class="btn btn-default pull-left home"><a href="<?php echo url() ?>"><span class="icon-book"></span><span><?php echo l('start') ?></span></a></button>
+<a class="btn btn-default pull-left home" href="<?php echo url() ?>"><span class="icon-book"></span><span><?php echo l('start') ?></span></a>
 	<div class="btn-container">
 		<div class="btn btn-default btn-icon center-block noHover">
 			<div class="icon">
@@ -11,18 +11,22 @@
 
 			<h2 class=" center-block"><?php echo strip_tags($page->title()) ?></h2>
 		</div>
-	</div>  
-	<ul class="list-group" role="navigation">
+	</div>
+
+	<ul class="list-group">
 		<?php foreach($page->children()->visible() as $child) :?>
-			<a href="<?php echo $child->url() ?>" target="_self">
+			
 				<li class="list-group-item">
-						<div class="text-container noOverflow">
-							<span class=""><?php echo $child->title() ?></span>
-						</div>
+				<a class="text-container noOverflow" href="<?php echo $child->url() ?>" target="_self">
+						
+							<?php echo $child->title() ?>
+						
+				</a>		
 				</li>    
-			</a>
+			
 		<?php endforeach ?>  
 	</ul>
+
 </div>
 <?php snippet('footer'); ?>
 
