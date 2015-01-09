2a888cfe68adaed659b4dfd41b3035d9423746a6
Sonja Broda <sonja@imap.cc>
2015-01-09 11:38:00 +0100

    add center-block class to icon

--- site/templates/about.php
+++ site/templates/about.php
@@ -2,7 +2,7 @@
 	<div id="main-content" class="container main-content" role="main">
 	<a class="btn btn-default pull-left home" href="<?php echo url() ?>"><span class="icon-book"></span><span><?php echo l('start') ?></span></a>
 		<div class="btn-container">
-			<div class="btn btn-default btn-icon noHover">
+			<div class="btn btn-default btn-icon center-block noHover">
 				<div class="icon">
 					<span class="icon-<?php echo $page->icon() ?>"></span>
 				</div>					    
